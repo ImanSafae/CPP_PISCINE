c5d0bfa9190c5fe8491e5cd48b872d4e1f19c3e9
IT <it@iMac-de-IT.local>
2023-01-31 02:28:30 +0100

    shrubbery form done

--- CPP_05/ex02/RobotomyRequestForm.hpp
+++ CPP_05/ex02/RobotomyRequestForm.hpp
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "Form.hpp"
+
+class   RobotomyRequestForm : public Form
+{
+public:
+    RobotomyRequestForm( void );
+    RobotomyRequestForm( std::string target );
+    RobotomyRequestForm( RobotomyRequestForm const &src );
+    ~RobotomyRequestForm( void );
+
+    RobotomyRequestForm &operator=( RobotomyRequestForm const &src );
+
+    virtual void    beSigned( Bureaucrat &employee );
+
+private:
+    std::string _target;
+};
