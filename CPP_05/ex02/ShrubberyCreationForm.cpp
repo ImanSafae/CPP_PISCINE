2f47146b979aac4db7374bc38ea736560d30a39b
Iman <iman-104@hotmail.fr>
2023-02-07 13:59:01 +0100

    day 05 done

---
+++ CPP_05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.cpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaouil <itaouil@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/27 18:23:46 by itaouil           #+#    #+#             */
+/*   Updated: 2023/01/27 18:24:15 by itaouil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm( void ) :
--- CPP_05/ex02/ShrubberyCreationForm.hpp
+++ CPP_05/ex02/ShrubberyCreationForm.hpp
@@ -5,6 +5,12 @@
 class   ShrubberyCreationForm : public Form
 {
 public:
+	ShrubberyCreationForm( void );
+	ShrubberyCreationForm( ShrubberyCreationForm const &src );
+	~ShrubberyCreationForm( void );
 
+	ShrubberyCreationForm &operator=( ShrubberyCreationForm const &src );
+
+	virtual void	beSigned( Bureaucrat &employee );
 private:
 };
