cfe24a18f9c7f213fd6b4e3bf279606c2afa4a3a
ImanSafae <iman-104@hotmail.fr>
2023-02-07 03:52:12 +0100

    day06 started

---
+++ CPP_06/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		std::cout << "Wrong number of arguments. Please only send one argument of char, int, float or double type." << std::endl;
+		return (0);
+	}
+	
+}
