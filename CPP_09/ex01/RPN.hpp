5dc445b8df00d348d6fb9f691557f4008d1c7878
ImanSafae <iman-104@hotmail.fr>
2023-03-06 01:18:48 +0100

    day 9 ex1 done

---
+++ CPP_09/ex01/RPN.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <stdlib.h>
+#include <cstdlib> 
+#include <sstream>
+
+void    rpn(std::string request, std::stack<std::string> &rpnstack);
